e51dfe130ece999317fdc18979bcd51f68fc2c48
saraivais <isadorasaraiva95@gmail.com>
2022-10-22 01:57:50 -0300

    feat [components]: adds evolution chain template~

---
+++ src/components/EvolutionChain.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="evolutions-container">
+    <div class="evolutions" v-for="(evolution, index) in this.chain" v-bind:key="index">
+      <div class="unit-evolution">
+        <h1 class="evo-name">{{ evolution.name }}</h1>
+        <img class="evo-image" :src="evolution.sprites.front_default" alt="evo-img">
+        <button class="chose-btn" v-on:click="this.changeFunction(evolution)">Escolho você!</button>
+      </div>
+    </div>
+  </div>
+</template>
+
