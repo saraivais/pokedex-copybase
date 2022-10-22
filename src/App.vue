ea043472461b4efb99e9485c6dfcc34fc628b7e2
saraivais <isadorasaraiva95@gmail.com>
2022-10-22 01:37:58 -0300

    feat [app]: adds html template~

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <HeaderComponent />
+  <div class="pokedex-container">
+    <div class="search-area">
+      <input class="search-input" type="text" v-model="pokemonName">
+      <button class="search-btn" v-on:click="getAllPokemonInfo()">Buscar</button>
+    </div>
+
+    <div class="display-area">
+    <PokemonDisplay v-if="readyToDisplay"
+      :pokemonObject="this.pokemonToDisplay"
+    />
+    <div class="not-found" v-if="searchError">
+      <PokemonNotFound />
+    </div>
+
+    <EvolutionChain
+      v-if="readyToDisplay"
+      :chain="this.evolutions"
+      :changeFunction="this.setDisplayPokemon"
+    />
+    </div>
+
+  </div>
+  <FooterComponent />
+</template>
