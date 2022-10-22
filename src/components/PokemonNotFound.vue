093f2fd3e8f3f6eb2a8189291ab4ea4b9f4b6fc1
saraivais <isadorasaraiva95@gmail.com>
2022-10-22 01:46:45 -0300

    feat [components]: adds not found component~

---
+++ src/components/PokemonNotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <img class="not-found-image" src="https://kuuiz.com/wp-content/uploads/2016/10/Pokemon-Silhouette-132-Ditto-370x297.png" alt="ditto-sillhouete">
+  <h1 class="not-found-title">Este pokemon não existe!</h1>
+</template>
+
+<style scoped>
+.not-found-image {
+  width: 200px;
+}
+
+.not-found-title {
+  color: red;
+}
+</style>
